3c5bb7113814163603c88af8332b88ee3e531fa5
Rinki <rinki@mapmyindia.com>
2024-05-21 14:01:41 +0530

    inital

---
+++ org/saga/byop/utility/SpoofEvaluator.py
@@ -0,0 +1,39 @@
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from tensorflow.keras.applications.mobilenet import preprocess_input
+
+class SpoofEvaluator:
+
+    def __init__(self, model,target_size=(180,180)):
+        self.model=model;
+        self.target_size=target_size
+
+    def preprocess_image(self,image_path):
+        img = load_img(image_path,target_size=self.target_size)
+        img_array = img_to_array(img)
+        img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+        img_array = img_array / 255.0  # Normalize the image
+
+    # this is  a function to predict the image label
+    def predict_image_label(self,image_path):
+
+        image_array=self.preprocess_image(image_path)
+
+        # Make predictions using your trained model
+        predictions = self.model.predict(image_array)
+
+        # Decode predictions
+        label_index = np.argmax(predictions)
+
+        # Depending on how you trained your model, you might have a label mapping to decode the index
+        # If you have a label mapping, you can use it to get the label name
+        label_name = "Your Label"  # Replace this with your actual label name
+        confidence = predictions[0][label_index]  # Get confidence score for the predicted label
+        confidence_percent=confidence*100
+        label_name="REAL"
+        if(confidence_percent<50):
+
+
+            label_name="SPOOF"
+
+        return label_name, confidence_percent
