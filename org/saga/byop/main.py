a521f2e3320588847b5e2f7aac58b8cb0a814fbd
Rinki <rinki@mapmyindia.com>
2024-05-21 18:54:25 +0530

    inital

--- org/saga/byop/main.py
+++ org/saga/byop/main.py
@@ -21,7 +21,9 @@ class main:
         # image_capturing.image_capturing.start_capturing_images_from_vcam(directory_path,10)
 
         # reading the captured images
-        Helper.get_image_files_from_dir(directory_path)
+        image_paths = Helper.load_images_from_dir(directory_path)
+
+        print(image_paths)
 
         # 2-load the model
         print("Loading Model");
@@ -34,16 +36,20 @@ class main:
         image_path = '/content/drive/MyDrive/data/spoof_980.png'
 
         # 4-predict image for real or spoof
-        label_name, confidence = evaluator.predict_image_label(image_path)
-        print("**********Prediction for the image*************:", label_name)
-        print("Confidence:", confidence)
-
+        label_names = evaluator.predict_images_labels(image_paths)
+        for label_name in label_names:
+            print(label_name)
+        Helper.likelihood_estimator(label_names, "REAL", "SPOOF")
         # 5-match the image for similarity
-        image_to_verify = '/content/drive/MyDrive/data/A1.jpg'
+        # image_to_verify = '/content/drive/MyDrive/data/A1.jpg'
+        results = []
         reference_image = '/content/drive/MyDrive/data/A2.jpg'
-        Helper.plot_comparing_images(image_to_verify, reference_image)
-        result = image_similarity_matcher.image_similarity_match(image_to_verify, reference_image)
-        print("**********Similarity Results*************:", result)
+        for image_to_verify in image_paths:
+            Helper.plot_comparing_images(image_to_verify, reference_image)
+            result = image_similarity_matcher.image_similarity_match(image_to_verify, reference_image)
+            print("Result of Similarity is ", result)
+            results.append(result)
+        Helper.likelihood_estimator(results, "True", "False")
 
 
 m = main()
