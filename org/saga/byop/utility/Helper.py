0af185550674f70319425b4629a469b6efb765be
Rinki <rinki@mapmyindia.com>
2024-05-21 18:57:19 +0530

    inital

--- org/saga/byop/utility/Helper.py
+++ org/saga/byop/utility/Helper.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 
 class Helper:
 
-  def get_image_files_from_dir (directory_path):
-      for filename in os.listdir(directory_path):
-          file_path = os.path.join(directory_path, filename)
-          if os.path.isfile(file_path):
-              print(f'Processing file: {filename}')
+  def load_images_from_dir(directory):
+    path = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".jpg") or filename.endswith(".png"):  # Adjust file extensions as needed
+            filepath = os.path.join(directory, filename)
+            path.append(filepath)
+    return path
 
   def plot_comparing_images(image_to_verify,reference_image):
       img1 = cv2.imread(image_to_verify)
@@ -19,3 +21,16 @@ class Helper:
       plt.imshow(img2)
       plt.axis('off')  # Hide axis
       plt.show()
+
+  def likelihood_estimator(data_array,value1,value2):
+          # Count occurrences of each value
+      value1_count = data_array.count(value1)
+      value2_count = data_array.count(value2)
+
+      # Compare counts
+      if value1_count > value2_count:
+          print("There are more {value1} values.")
+      elif value1_count < value2_count:
+          print(f"There are more {value2} values.")
+      else:
+          print(f"There are an equal number of {value1} and {value2} values: {count_value1} each.")
