00777299fab1d5a63feedfe38d27aba8a4bc1ec6
Rinki <rinki@mapmyindia.com>
2024-05-24 17:40:59 +0530

    inital

--- org/saga/byop/training/face_detection.py
+++ org/saga/byop/training/face_detection.py
@@ -8,12 +8,13 @@ from org.saga.byop.training.face_landmarks_detection import face_landmarks_detec
 
 class  face_detection:
 
-    def face_detector(self,blink_detection=True,landmark_detection=True):
+    def face_detector(self,wait_time=1000,frame_count=5,blink_detection_status=True,landmark_detection_status=True):
         # Open webcam
         cap = cv2.VideoCapture(0)
         detector = dlib.get_frontal_face_detector()
-        while True:
-            ret, frame = cap.read()
+        ret, frame = cap.read()
+
+        while frame>=frame_count:
             if not ret:
                 break
 
@@ -22,20 +23,20 @@ class  face_detection:
 
             # Detect faces in the grayscale frame
             faces = detector(gray)
-            if (blink_detection):
+            if (blink_detection_status):
                 blink = blink_detection(config_manager.get_face_detection_model_path());
-            if (blink_detection):
+            if (landmark_detection_status):
                 landmark = face_landmarks_detection(config_manager.get_face_detection_model_path());
 
             for face in faces:
-                if (blink_detection):
+                if (blink_detection_status):
                     blink.blink_detector(gray, face, frame)
-                if(landmark_detection):
+                if(landmark_detection_status):
                     landmark.face_landmark_detector(gray,face,frame)
 
             # Display the frame
             cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1) & 0xFF
+            key = cv2.waitKey(wait_time) & 0xFF
 
             # Break the loop if 'q' key is pressed
             if key == ord("q"):
